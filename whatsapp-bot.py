00bd4c1c0fef15d1d81c9d66f260e1ce81a5fad8
Rahul Gohil <rahul.gohil@workmail.com>
2020-06-22 15:33:20 +0530

    Add files via upload

--- whatsapp-bot.py
+++ whatsapp-bot.py
@@ -1,46 +1,75 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from message import format
-import time
+import functools
 import codecs
+import time
+
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys          import Keys
+from selenium.webdriver.common.by            import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support    import expected_conditions as EC
+
+from selenium import webdriver
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+open_file = functools.partial(open, encoding='utf8')
+
+def format():
+    message = open_file('message.txt', 'r').readlines()
+    formatted = ''
+    
+    for i in range(len(message)):
+        formatted += message[i].rstrip('\n') + " " + "\\n"
+
+    with open_file("message_formatted.txt", "w") as final:
+        final.writelines(formatted)
+
+    print("Message formating complete")
 
 print("*** WhatsApp Bot started ***")
 format()
-driver = webdriver.Chrome()
-print("Login to WhatsApp Web and press ENTER only once logged in.")
+
+driver = webdriver.Chrome(
+    ChromeDriverManager().install()    
+)
+
 time.sleep(3)
-whatsapp_url = "https://web.whatsapp.com"
-driver.get(whatsapp_url)
-input()
+driver.get("https://web.whatsapp.com")
+
 with open("numbers.txt", "r") as numbers:
     for number in numbers:
         number = number.strip()
         if len(number) < 11:
             print("Invalid Number")
         else:
-            url = "https://web.whatsapp.com/send?phone=" + number
-            driver.get(url)
+            driver.get("https://web.whatsapp.com/send?phone=" + number)
             element = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located(
-                    (By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'))
+                    (By.XPATH, 
+                    '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+                )
             )
-            # time.sleep(20)
-            input_ = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-            file = open("message_formatted.txt", "r", encoding="utf8")
-            x = file.read()
-            # x = codecs.decode(x, 'unicode_escape')
-            for y in x.split(r'\n'):
+            message_box = driver.find_element_by_xpath(
+                '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
+            ) 
+            for line in open_file(
+                "message_formatted.txt",
+                "r",
+            ).read().split(r'\n'):
                 try:
-                    input_.send_keys(y)
-                    ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.SHIFT).key_up(
-                        Keys.ENTER).perform()
+                    message_box.send_keys(line)
+                    ActionChains(driver)  \
+                    .key_down(Keys.SHIFT) \
+                    .key_down(Keys.ENTER) \
+                    .key_up(Keys.SHIFT)   \
+                    .key_up(Keys.ENTER)   \
+                    .perform()
                 except:
                     print("Message format Error")
                     quit()
-            driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
+            driver.find_element_by_xpath(
+                '//*[@id="main"]/footer/div[1]/div[3]/button'
+            ).click()
             print("Message sent to " + number)
             time.sleep(2)
